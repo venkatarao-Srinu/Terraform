65d1c7ad4073db813b57b4d834eda2ee0a6d3cd8
venkatarao-Srinu <venkataraosrinu12@gmail.com>
2025-12-17 00:36:40 +0530

    Load balancer and DNS Routing

--- main.tf
+++ main.tf
@@ -207,3 +207,103 @@ data "aws_ami" "amazon_linux_2" {
     values = [var.ami_name_filter]
   }
 }
+
+resource "aws_security_group" "dev_alb_sg" {
+  name        = "dev-alb-sg"
+  description = "ALB Security Group"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "dev-alb-sg"
+  }
+}
+
+resource "aws_lb" "dev_alb" {
+  name               = "dev-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.dev_alb_sg.id]
+  subnets            = [
+    aws_subnet.public[0].id,
+    aws_subnet.public[1].id
+  ]
+
+  tags = {
+    Name = "dev-alb"
+  }
+}
+
+resource "aws_lb_target_group" "dev_tg" {
+  name     = "dev-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id  = aws_vpc.this.id
+
+  health_check {
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "dev-tg"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_1" {
+  target_group_arn = aws_lb_target_group.dev_tg.arn
+  target_id        = aws_instance.dev_web_1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web_2" {
+  target_group_arn = aws_lb_target_group.dev_tg.arn
+  target_id        = aws_instance.dev_web_2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.dev_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.dev_tg.arn
+  }
+}
+
+data "aws_route53_zone" "srinuapps" {
+  name         = "srinuapps.shop"
+  private_zone = false
+}
+
+resource "aws_route53_record" "test2" {
+  zone_id = data.aws_route53_zone.srinuapps.zone_id
+  name    = "test2.srinuapps.shop"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.dev_alb.dns_name
+    zone_id                = aws_lb.dev_alb.zone_id
+    evaluate_target_health = true
+  }
+}
